947d201960051fe131b5cffe6a54dac1bca50a4d
zucchinidev <azga84@gmail.com>
2021-03-23 21:54:35 +0100

    This commit will add the necessary logic to implement the solution to reddit and hacker news client in a concurrent way

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.13
 
 require (
 	github.com/beefsack/go-rate v0.0.0-20200827232406-6cde80facd47 // indirect
-	github.com/caser/gophernews v0.0.0-20150107061403-f17255d5b792 // indirect
+	github.com/caser/gophernews v0.0.0-20150107061403-f17255d5b792
 	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/jzelinskie/geddit v0.0.0-20200521013404-78c28c13fba2 // indirect
+	github.com/jzelinskie/geddit v0.0.0-20200521013404-78c28c13fba2
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
 	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84 // indirect
 )
--- reddit/main.go
+++ reddit/main.go
@@ -2,105 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/caser/gophernews"
-	"github.com/jzelinskie/geddit"
+	"github.com/zucchinidev/hands-on-concurrency-go/reddit/hnClient"
+	"github.com/zucchinidev/hands-on-concurrency-go/reddit/redditClient"
+	"github.com/zucchinidev/hands-on-concurrency-go/reddit/story"
 	"os"
 )
 
-var redditSession *geddit.LoginSession
-var hackerNewsClient *gophernews.Client
-
-func init() {
-	// hacker news API allows use it without authentication
-	hackerNewsClient = gophernews.NewClient()
-	var err error
-	redditSession, err = geddit.NewLoginSession("g_d_bot", "K417k4FTua52", "gdAgent v0")
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func toStdout(c <-chan story.Story)  {
+	for {
+		s := <-c
+		fmt.Printf(stringTmpl(), s.Title, s.URL, s.Author, s.Source)
 	}
 }
 
-type Story struct {
-	title  string
-	url    string
-	author string
-	source string
-}
-
-func newHackerNewsStories() []Story {
-	var stories []Story
-	changes, err := hackerNewsClient.GetChanges()
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	for _, id := range changes.Items {
-		story, err := hackerNewsClient.GetStory(id)
-		if err != nil {
-			// some ids are comments, we don't care about that
-			continue
-		}
-		stories = append(stories, Story{
-			title:  story.Title,
-			url:    story.URL,
-			author: story.By,
-			source: "HackersNews",
-		})
-	}
-	return stories
-}
-
-func newRedditStories() []Story {
-	var stories []Story
-	sort := geddit.PopularitySort(geddit.NewSubmissions) // more recently stories
-	var listingOptions geddit.ListingOptions
-	submissions, err := redditSession.SubredditSubmissions("programming", sort, listingOptions)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	for _, s := range submissions {
-		stories = append(stories, Story{
-			title:  s.Title,
-			url:    s.URL,
-			author: s.Author,
-			source: "Reddit /r/programming",
-		})
-	}
-	return stories
-}
-
-func main() {
-	hnStories := newHackerNewsStories()
-	redditStories := newRedditStories()
-
-	var stories []Story
-
-	if hnStories != nil {
-		stories = append(stories, hnStories...)
-	}
-
-	if redditStories != nil {
-		stories = append(stories, redditStories...)
-	}
-
-	f, err := os.Create("stories.txt")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer f.Close()
-
-	for _, story := range stories {
-		fmt.Fprintf(f, stringTmpl(), story.title, story.url, story.author, story.source)
-	}
-
-	for _, story := range stories {
-		fmt.Printf(stringTmpl(), story.title, story.url, story.author, story.source)
+func toFile(c <-chan story.Story, f *os.File)  {
+	for {
+		s := <-c
+		_, _ = fmt.Fprintf(f, stringTmpl(), s.Title, s.URL, s.Author, s.Source)
 	}
 }
 
@@ -110,3 +28,44 @@ by %s on %s
 
 `
 }
+func main() {
+	fromHN := make(chan story.Story, 8)
+	fromReddit := make(chan story.Story, 8)
+	toFileCh := make(chan story.Story, 8)
+	toStdoutCh := make(chan story.Story, 8)
+
+
+	go hnClient.HackerNewsStories(fromHN)
+	go redditClient.RedditStories(fromReddit)
+
+	f, err := os.Create("stories.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	go toStdout(toStdoutCh)
+	go toFile(toFileCh, f)
+
+	hnOpen, redditOpen := true, true
+	var s story.Story
+
+	for hnOpen || redditOpen {
+		select {
+		case s, hnOpen = <-fromHN:
+			if hnOpen {
+				toFileCh <- s
+				toStdoutCh <- s
+			}
+
+		case s, redditOpen = <-fromReddit:
+			if redditOpen {
+				toFileCh <- s
+				toStdoutCh <- s
+			}
+		}
+	}
+}
+
+
